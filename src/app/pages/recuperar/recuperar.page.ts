fe4ffc36b9916a73a6dcb83db8f7e04f3d0532cc
Belen Ruz <bel.ruz@duocuc.cl>
2024-09-09 14:06:01 -0300

    Actualizacion en recuperar
    Según las instrucciones de la prueba en recuperar debe ingresar el nombre de usuario y hacer clic en botón recuperar, lo que lo lleva a la pagina home, así que lo deje así.

--- src/app/pages/recuperar/recuperar.page.ts
+++ src/app/pages/recuperar/recuperar.page.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-
+import { ActivatedRoute, Router } from '@angular/router'; 
 
 @Component({
   selector: 'app-recuperar',
@@ -10,12 +9,15 @@ import { ActivatedRoute } from '@angular/router';
 export class RecuperarPage implements OnInit {
   username: string = ''; // Inicializar con una cadena vacía
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private router: Router) {} 
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
       this.username = params['username'] || ''; // Obtener el nombre de usuario de los parámetros de la URL
     });
   }
- 
+
+  Home() {
+    this.router.navigate(['/home']);
+  }
 }
